e9be902e071b3e5270d010659da714cbe73d956c
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-12 22:28:09 -0300

    Solved

--- FizzBuzz/Ṕython/1.py
+++ FizzBuzz/Ṕython/1.py
@@ -1,49 +1,33 @@
-list = []
+c5 = 0
 
-a = True
+d3 = 0
 
-b = 0
+e35 = 0
 
-c = 0
-
-d = 0
-
-e = 0
-
-while a == True:
-
-   list.append(b+1)
+for b in range(101):
 
    if b % 3 == 0 and b % 5 == 0 and b != 0:
 
       print(b, "is FizzBuzz")
 
-      d += 1
+      d3 += 1
 
-      c += 1
+      c5 += 1
 
-      e += 1
+      e35 += 1
 
    elif b % 3 == 0:
 
-      d += 1
+      d3 += 1
 
    elif b % 5 == 0:
 
-      c += 1
-
-   if b == 100:
-
-      a = False
-
-   else:
-
-      b += 1
+      c5 += 1
 
 print()
 
-print("Has", d, "Fizz numbers")
+print("Has", d3, "Fizz numbers")
 
-print("Has", c, "Buzz numbers")
+print("Has", c5, "Buzz numbers")
 
-print("Has", e, "FizzBuzz numbers")
+print("Has", e35, "FizzBuzz numbers")
