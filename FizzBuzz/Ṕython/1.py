e33f47ec9edb1dcf0c1629f338f84474a70e64cc
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-12 21:45:17 -0300

    test

--- FizzBuzz/Ṕython/1.py
+++ FizzBuzz/Ṕython/1.py
@@ -1,2 +1,45 @@
-a=1
+import operator
 
+list = []
+
+a = True
+
+b = 0
+
+c = 0
+
+d = 0
+
+while a == True:
+
+   list.append(b+1)
+
+   if b % 3 == 0:
+
+      print(b % 3 == 0)
+
+      ##print("Fizz")
+
+      print(d)
+
+   elif b % 5 == 0:
+
+         c += 1
+
+        ## print("Buzz")
+
+   else:
+
+      print(b, " Isn't Fizz Nor Buzz")
+
+   if b == 100:
+
+      a = False
+
+   else:
+
+      b += 1
+
+print("Has ", d, " Fizz numbers")
+
+print("Has ", c, " Buzz numbers")
