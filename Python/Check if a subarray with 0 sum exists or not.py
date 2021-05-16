12fa7887d078fa3d2d6fd00e4b03b41c278f46d5
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-16 14:39:19 -0300

    Big improvements to solve the Problem

--- Python/Check if a subarray with 0 sum exists or not.py
+++ Python/Check if a subarray with 0 sum exists or not.py
@@ -1,11 +1,54 @@
 # Source: https://www.techiedelight.com/check-subarray-with-0-sum-exists-not/
 
+# Fix the variables names of past files;
+
 import random
 
-s = {random.randint(-30, 30)}
+SubSet0 = {0}
 
-for a in range(0, 15):
+SubSet1 = {0}
 
-    s.add(random.randint(-30, 30))
+SubSet2 = {0}
 
-print(s)
+MasterSet = {0}
+
+a = 0
+
+max = 15
+
+while a != max/3:
+
+    if not SubSet0.clear():
+
+        SubSet0.remove(0)
+
+    else:
+
+        SubSet0.add(random.randint(-30, 30))
+
+    if not SubSet1.clear():
+
+        SubSet1.remove(0)
+
+    else:
+
+        SubSet1 = random.randint(-30, 30)
+
+    if not SubSet2.clear():
+
+        SubSet2.remove(0)
+
+    else:
+        SubSet2 = random.randint(-30, 30)
+
+    a += 1
+
+MasterSet.remove(0)
+
+MasterSet.add(SubSet0)
+
+MasterSet.add(SubSet1)
+
+MasterSet.add(SubSet2)
+
+print(MasterSet)
