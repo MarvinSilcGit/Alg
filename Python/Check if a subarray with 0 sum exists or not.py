acfbfe9a61d2d68bf546b9ac92d91b9128545f13
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-17 15:19:54 -0300

    Incosistencies to solve and Big changes

--- Python/Check if a subarray with 0 sum exists or not.py
+++ Python/Check if a subarray with 0 sum exists or not.py
@@ -18,33 +18,25 @@ max = 15
 
 while a != max/3:
 
-    if 31 in SubSet0.clear():
+    if a == 0:
 
-        SubSet0.remove(31)
+        SubSet0.clear()
+
+        SubSet1.clear()
+
+        SubSet2.clear()
 
     else:
 
         SubSet0.add(random.randint(-30, 30))
 
-    if 31 in SubSet1.clear():
+        SubSet1.add(random.randint(-30, 30))
 
-        SubSet1.remove(31)
-
-    else:
-
-        SubSet1 = random.randint(-30, 30)
-
-    if 31 in SubSet2.clear():
-
-        SubSet2.remove(31)
-
-    else:
-
-        SubSet2 = random.randint(-30, 30)
+        SubSet2.add(random.randint(-30, 30))
 
     a += 1
 
-MasterSet.remove(0)
+MasterSet.remove(31)
 
 MasterSet.add(SubSet0)
 
