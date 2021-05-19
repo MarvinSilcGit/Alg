c0f064e2188c50d5e8d926bf8454ca48761aa417
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-19 18:46:25 -0300

    modifying the spacing between variables, operators and keywords

--- Python/2018-2019/Exercício 5.23.24.py
+++ Python/2018-2019/Exercício 5.23.24.py
@@ -1,15 +1,29 @@
-cont=0
+cont = 0
+
 while True:
+
     print()
-    primo=int(input("Digite um número para saber se ele é primo ou não: "))
-    if primo%2==0 and primo!=2:
+
+    primo = int(input("Digite um número para saber se ele é primo ou não: "))
+
+    if primo % 2 == 0 and primo != 2:
+
         print()
-        print("O número %d não é primo"%primo)
+
+        print("O número %d não é primo" % primo)
+
         continue
-    if primo==2:
-        cont=cont+1
+
+    if primo == 2:
+
+        cont = cont+1
+
     else:
-        primos=(primo%2)
-        cont+=1
+
+        primos = (primo % 2)
+
+        cont += 1
+
     print()
-    print("%d é um número primo"%primo)
+
+    print("%d é um número primo" % primo)
