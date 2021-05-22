86841b605dda6cbfc02cb5e9a255c0f511cb74f3
MarvinSilc <marvinsilc@protonmail.ch>
2021-05-21 23:12:11 -0300

    adding new file

---
+++ Python/2021/Find maximum length subarray having a given sum.py
@@ -0,0 +1,7 @@
+# Source https://www.techiedelight.com/find-maximum-length-sub-array-having-given-sum/
+
+import random
+
+Counter, Counter1, Array = 0, 0, []
+
+# To divide the array in subarray and ask a number to find out wich subarray combination macth with the number asked
