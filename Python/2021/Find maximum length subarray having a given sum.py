01255fdf63f1beabaf89ba4e07157e5fce0de9d1
MarvinSilc <marvinsilc@protonmail.ch>
2021-07-14 10:14:42 -0300

    modifications

--- Python/2021/Find maximum length subarray having a given sum.py
+++ Python/2021/Find maximum length subarray having a given sum.py
@@ -2,7 +2,7 @@
 
 import random
 
-Counter1, Counter2, Counter3, Sum, Array, RandomLen, SubArray, SubArray1 = 0, 1, 2, 0, [], [], [], []
+Counter1, Counter2, Sum, Array, RandomLen, SubArray = 0, 1, 0, [], [], []
 
 Goal = random.randint(1, 30)
 
@@ -20,47 +20,29 @@ for Counter in range(15):
 
 while Counter1 != len(Array) - 1:
 
-    if Counter1 + 1 == Counter2:
+    if Array[Counter1] + Array[Counter2] <= sum:
 
-        Sum = Counter1 + Counter2
+        SubArray.append(Array[Counter2])
 
-    if Sum <= Goal:
+    Counter1 += 1
 
-        SubArray1.append(Array[Counter2])
-
-        if Sum == Goal:
-
-            Sum = Counter1 + Counter2
-
-    Counter3 += 1
-
-    if Counter3 == len(Array) and Counter2 != len(Array):
-
-        Counter2 += 1
-
-        Counter3 = Counter2 + 1
-
-        Sum = Counter1 + Counter2
-
-    if Counter2 == len(Array) - 1:
-
-        if Sum == Goal:
-
-            while len(SubArray1) != 0:
-
-                SubArray.append(SubArray1.pop(0))
+    if Counter1 == len(Array):
 
         Counter1 += 1
 
         Counter2 = Counter1 + 1
 
-        Counter3 = Counter2 + 1
-
         Sum = 0
 
-        del SubArray1[:]
+        del SubArray[:]
 
 print()
 print(Array)
 print(Goal)
 print(SubArray)
+
+# 25
+# 10 14 15 5 1 4 2 6
+# 24
+# 2, 6, 10, 9, 11, 15, 12, 13
+# Sum the actual number with next number. If the next number minus past number has a value closest, remove the past and add the new one;
