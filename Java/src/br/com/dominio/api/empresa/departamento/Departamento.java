9655f0aad720a17dc94b26fa1a6f6fe23f09e568
marvin <MarvinSilcGit>
2021-08-12 16:23:52 -0300

    Aprimorando e modificando o projeto original / improving and modifying the original project

--- Java/src/br/com/dominio/api/empresa/departamento/Departamento.java
+++ Java/src/br/com/dominio/api/empresa/departamento/Departamento.java
@@ -1,5 +1,6 @@
 package br.com.dominio.api.empresa.departamento;
 
+
 import java.util.ArrayList;
 
 //Fim dos Imports / End of Imports;
@@ -9,11 +10,13 @@ public class Departamento
 {
     private String nome;
 
-    private ArrayList listaFuncionários = new ArrayList(50);
-
     private Diretor diretor;
 
     private ViceDiretor viceDiretor;
+
+    private double orcamentoTrimestral;
+
+    private ArrayList listaFuncionários = new ArrayList(50);
 
     //Fim do Campo de Declaração de Atributos / End of Attributes Declaration Field;
 
@@ -24,14 +27,57 @@ public class Departamento
     }//Fim do Construtor Padrão / End of Default Constructor;
 
 
-    public Departamento(String nome, Diretor diretor, ViceDiretor viceDiretor)
+    public Departamento(String nome, Diretor diretor, ViceDiretor viceDiretor, double orcamentoTrimestral)
     {
         this.nome = nome;
 
         this.diretor = diretor;
 
         this.viceDiretor = viceDiretor;
+
+        this.orcamentoTrimestral = orcamentoTrimestral;
     }//Fim do Construtor Personalizado / End of Personalized Constructor;
 
+
+    public final String retornarNome()
+    {
+        return nome;
+    }
+
+    public final String retornarDiretor()
+    {
+        return diretor.retornarNome();
+    }
+
+    public final String retornarViceDiretor()
+    {
+        return viceDiretor.retornarNome();
+    }
+
+    public final double retornarOrcamentoTrimestral()
+    {
+        return orcamentoTrimestral;
+    }
+    //Fim do Métodos Retornadores / End of Getter Methods;
+
+    public final void alterarNome(String novoNome)
+    {
+        nome = novoNome;
+    }
+
+    public final void alterarDiretor(Diretor novoDiretor)
+    {
+        diretor = novoDiretor;
+    }
+
+    public final void alterarViceDiretor(ViceDiretor novoViceDiretor)
+    {
+        viceDiretor = novoViceDiretor;
+    }
+
+    public final void alterOrcamentoTrimestral(double novoOrcamento)
+    {
+        orcamentoTrimestral = novoOrcamentoTrimestral;
+    }
 
 }//Fim da Classe / End of Class;
---
+++ Java/src/br/com/dominio/api/empresa/departamento/Despesas.java
@@ -0,0 +1,20 @@
+package br.com.dominio.api.empresa.departamento;
+
+import java.util.Dictionary;
+
+//Fim dos Imports / End of Imports;
+
+
+public class Despesas
+{
+    //Colocar dicionário para que a despesa esteja relacionada ao valor gasto.
+    // Ligar as despesas com o valor do orcamento, impedindo que as despesa superem o valor do orcamento.
+    // Se o valor do orcamento for alterado para menos que o valor das despesas, a classe despesas tem que emitir um aviso de revisão dos gastos;
+
+    public Despesas()
+    {
+
+    }//Fim do Construtor Padrão / End of Default Constructor;
+
+
+}//Fim da Classe / End of Class;
