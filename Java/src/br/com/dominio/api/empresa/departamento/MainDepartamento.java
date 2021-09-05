26f93d7fb1a2f22fe3b20984d3657dfc8cd6637e
marvin <MarvinSilcGit>
2021-09-05 14:10:49 -0300

    Aprimorando / improving

--- Java/src/br/com/dominio/api/empresa/departamento/MainDepartamento.java
+++ Java/src/br/com/dominio/api/empresa/departamento/MainDepartamento.java
@@ -3,9 +3,9 @@ package br.com.dominio.api.empresa.departamento;
 
 public final class MainDepartamento
 {
-    public final static void main (String [] args)
+    public static void main (String [] args)
     {
-
+        System.out.println("ss");
     }
     //Fim do Método Main / End of Main Method;
 
